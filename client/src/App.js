910fc5f904e9c189da04b19baafcf1ac9064b46c
Thomas Boehm <thomas.boehm@tum.de>
2021-10-07 17:02:11 +0200

    Setup the server for classification

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,46 @@
 import axios from 'axios';
 import { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [imgFile, setImgFile] = useState();
+  
+  function onFileChange(event) {
+    console.log(event.target.files[0]);
+    setImgFile(event.target.files[0])
+  }
+
+  async function onFileUpload() {
+    if (!imgFile) {
+      return;
+    }
+    
+    const formData = new FormData();
+    formData.append(
+      "file",
+      imgFile,
+      imgFile.name
+    );
+
+    try {
+      const res = await axios.post("http://127.0.0.1:5000/", formData);
+      console.log(res.data);
+    } catch (e) {
+      console.log('Error: ' + e);
+    }
+  }
+
   return (
-    <div className="App">
-      hello world
+    <div>
+        <h1>
+          Pokemon Classifier
+        </h1>
+        <h3>
+          Upload your image
+        </h3>
+        <div>
+          <input type="file" onChange={onFileChange} />
+          <button onClick={onFileUpload}>Upload</button>
+        </div>
     </div>
   );
 }
-
-export default App;
--- server/server.py
+++ server/server.py
@@ -1,11 +1,33 @@
-from flask import Flask, request, jsonify
+import os
+from flask import Flask, abort, request, jsonify
+from flask_cors import cross_origin
+from werkzeug.utils import secure_filename
+
+from classifier import Classifier
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './uploads'
+
+classifier = Classifier()
 
 @app.route('/', methods=['GET', 'POST'])
+@cross_origin(origins=['http://localhost:3000'])
 def hello_world():
     if request.method == 'POST':
-        return jsonify(id=6, en='Charizard', de='Glurak')
+        file = request.files['file']
+        if file and file.filename:
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            
+            if not os.path.exists(app.config['UPLOAD_FOLDER']):
+                os.makedirs(app.config['UPLOAD_FOLDER'])
+            
+            file.save(filepath)
+            
+            pokemon_id, pokemon_en, pokemon_de = classifier.classify(filepath)
+            return jsonify(id=pokemon_id, en=pokemon_en, de=pokemon_de)
+        else:
+            abort(400)
     else:
         return 'Server running!'
 
