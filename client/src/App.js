15e69481e4ccbbd5b5a6f29be7d89df8560107f7
Thomas Boehm <thomas.boehm@tum.de>
2021-10-08 16:14:52 +0200

    Add styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,79 @@
-import axios from 'axios';
 import { useState } from 'react';
+import axios from 'axios';
+import logo from './resources/logo.svg';
+import './resources/styles.css';
+
+const SERVER_URL = 'https://ccf6-2001-a61-3462-cd01-45ca-9792-c7d2-cdad.ngrok.io/'
+const POKEMON_URL = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/'
 
 export default function App() {
-  const [imgFile, setImgFile] = useState();
-  
-  function onFileChange(event) {
-    console.log(event.target.files[0]);
-    setImgFile(event.target.files[0])
-  }
+  const [pokemon, setPokemon] = useState();
 
-  async function onFileUpload() {
-    if (!imgFile) {
+  async function submit(event) {
+    const img = event.target.files[0];
+    if (!img) {
       return;
     }
-    
+
     const formData = new FormData();
     formData.append(
       "file",
-      imgFile,
-      imgFile.name
+      img,
+      img.name
     );
 
     try {
-      const res = await axios.post("http://127.0.0.1:5000/", formData);
-      console.log(res.data);
+      const res = await axios.post(SERVER_URL, formData);
+      setPokemon(res.data)
     } catch (e) {
       console.log('Error: ' + e);
     }
   }
 
+  function resize(file) {
+    var img = document.createElement("img");
+    img.src = window.URL.createObjectURL(file);
+    var canvas = document.createElement('canvas'); 
+
+    var MAX_WIDTH = 800;
+    var MAX_HEIGHT = 600;
+    var width = img.width;
+    var height = img.height;
+
+    if (width > height) {
+      if (width > MAX_WIDTH) {
+        height *= MAX_WIDTH / width;
+        width = MAX_WIDTH;
+      }
+    } else {
+      if (height > MAX_HEIGHT) {
+        width *= MAX_HEIGHT / height;
+        height = MAX_HEIGHT;
+      }
+    }
+    canvas.width = width;
+    canvas.height = height;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, width, height);
+  }
+
   return (
-    <div>
-        <h1>
-          Pokemon Classifier
-        </h1>
-        <h3>
-          Upload your image
-        </h3>
-        <div>
-          <input type="file" onChange={onFileChange} />
-          <button onClick={onFileUpload}>Upload</button>
+    <div className="app">
+        <img className="logo" src={logo} alt="Pokemon Logo" />
+        <div className="info">
+          {pokemon ?
+            <div>
+              <h2>That's {pokemon.en.toUpperCase()}</h2>
+              <img className="pokemon-img" src={POKEMON_URL + `${pokemon.id}`.padStart(3, '0') + '.png'} alt={pokemon.en}/>
+            </div> :
+            <div>
+              <h2>Pokemon Classifier</h2>
+              <p>Select an Image of a Pokemon and see how it's called.</p>
+            </div>
+          }         
         </div>
+        <input className="inputfile" type="file" id="file" onChange={submit} accept=".jpg,.jpeg,.png,.svg" />
+        <label className="inputLabel" htmlFor="file">SELECT IMAGE</label>
     </div>
   );
 }
